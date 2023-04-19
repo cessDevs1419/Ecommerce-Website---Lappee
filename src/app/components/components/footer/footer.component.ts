5c13a63912387af53c5ea8745565f45159b8cd54
CorgiusMinimus <wendellravago@gmail.com>
2023-04-20 00:12:28 +0800

    update site title to static scope

--- src/app/components/components/footer/footer.component.ts
+++ src/app/components/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  siteTitle: string = AppComponent.title;
 }
