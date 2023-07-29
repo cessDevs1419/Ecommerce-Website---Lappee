bb40918988c2beb488b2cb0d85d0ef352f8f57d9
CorgiusMinimus <wendellravago@gmail.com>
2023-07-29 21:53:12 +0800

    Added image upload to review-form

--- src/app/components/components/modal-forms-client/review-form/review-form.component.ts
+++ src/app/components/components/modal-forms-client/review-form/review-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormArray } from '@angular/forms';
 import { OrderContent } from 'src/assets/models/order-details';
 
 @Component({
@@ -8,11 +9,22 @@ import { OrderContent } from 'src/assets/models/order-details';
 })
 export class ReviewFormComponent {
   @Input() product: OrderContent;
+  @ViewChild('attachmentUpload') imgInput: ElementRef;
   rating: number;
-  
+  files: string[] = [];
 
   ratingChange(rating: number){
     this.rating = rating;
     console.log(rating + " stars");
+  }
+
+  imgUpload(event: any): void{
+
+    let img: File = event.target.files[event.target.files.length - 1];
+    let reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.files.push(e.target.result);
+    }
+    reader.readAsDataURL(img);
   }
 }
