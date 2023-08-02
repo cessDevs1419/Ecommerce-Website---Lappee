ead5d55b4dc0ddeca44c7410dee79752be1de17e
CorgiusMinimus <wendellravago@gmail.com>
2023-08-02 10:06:51 +0800

    Updated subcategory page to use product images, removed discounts tag, made order count in profile banner dynamic, added Buy Now in product page, inquiry now works

--- src/app/components/components/profile-banner/profile-banner.component.ts
+++ src/app/components/components/profile-banner/profile-banner.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { AccountsService } from 'src/app/services/accounts/accounts.service';
+import { OrderService } from 'src/app/services/order/order.service';
 import { UsersService } from 'src/app/services/users/users.service';
-import { formatUser } from 'src/app/utilities/response-utils';
+import { formatOrderDetails, formatUser } from 'src/app/utilities/response-utils';
+import { OrderDetail } from 'src/assets/models/order-details';
 import { User } from 'src/assets/models/user';
 
 @Component({
@@ -15,7 +17,13 @@ export class ProfileBannerComponent {
   //user: User = this.accountService.user;
   //fullName: string = this.user.fname + " " + (this.user.mname ? this.user.mname : "") + " " + this.user.lname + " " + (this.user.suffix ? this.user.suffix : "");
   user: Observable<User> = this.accountService.getLoggedUser();
-  constructor(private accountService: AccountsService, private userService: UsersService) {}
+  orders: Observable<OrderDetail[]>;
+
+  constructor(private accountService: AccountsService, private userService: UsersService, private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    this.orders = this.orderService.getOrderDetailByUser().pipe(map((response: any) => formatOrderDetails(response)))
+  }
 
   /*
   users!: Observable<User[]>;
