1cf6f228a562c8cc98cef00ab75ce692a74337ab
CorgiusMinimus <wendellravago@gmail.com>
2023-11-13 18:10:35 +0800

    added support for order status 15 and 50

--- src/app/components/pages/main/order-details/order-details/order-details.component.ts
+++ src/app/components/pages/main/order-details/order-details/order-details.component.ts
@@ -38,6 +38,9 @@ export class OrderDetailsComponent {
       orders[0].order_contents.forEach((item: OrderContent) => {
         this.orderSubtotal += this.Number(item.sub_price)
       })
+      if(orders[0].status == 15 || orders[0].status == 50){
+        this.isCancelRequest = true;
+      }
     }) 
   }
 
