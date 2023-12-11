74f9643059826dda7fab8f38b31fa74ded942e42
CorgiusMinimus <wendellravago@gmail.com>
2023-12-11 16:44:48 +0800

    added content for shipping fee form

--- src/app/components/components/Forms/shipping-fee-form/shipping-fee-form.component.ts
+++ src/app/components/components/Forms/shipping-fee-form/shipping-fee-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { validateOptions } from 'pusher-js/types/src/core/options';
 
 @Component({
   selector: 'app-shipping-fee-form',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class ShippingFeeFormComponent {
 
+  shippingFeeForm = new FormGroup({
+    scope: new FormControl('', Validators.required),
+    provinces: new FormControl('', Validators.required),
+    price: new FormControl(0, Validators.required)
+  })
+
+  
 }
