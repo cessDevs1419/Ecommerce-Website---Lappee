23ac8bfbb50b85b2d2a1008603eb64f15df0420e
CorgiusMinimus <wendellravago@gmail.com>
2023-12-11 23:41:07 +0800

    fleshed out shipping fee form

--- src/app/components/components/Forms/shipping-fee-form/shipping-fee-form.component.ts
+++ src/app/components/components/Forms/shipping-fee-form/shipping-fee-form.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { validateOptions } from 'pusher-js/types/src/core/options';
+import * as bootstrap from 'bootstrap';
+import { ProvinceCityService } from 'src/app/services/province-city/province-city.service';
+import { Province } from 'src/assets/models/province-city';
 
 @Component({
   selector: 'app-shipping-fee-form',
@@ -9,11 +11,64 @@ import { validateOptions } from 'pusher-js/types/src/core/options';
 })
 export class ShippingFeeFormComponent {
 
+  constructor(private province: ProvinceCityService) {}
+
+  @ViewChild('tooltip') tooltip: ElementRef;
+  provinces: Province[] = []
+  selectedProvinces: Province[] = [];
+  provinceSelect: Province = {
+    name: '',
+    key: '',
+    region: ''
+  };
+
   shippingFeeForm = new FormGroup({
-    scope: new FormControl('', Validators.required),
-    provinces: new FormControl('', Validators.required),
-    price: new FormControl(0, Validators.required)
+    scope: new FormControl('general', Validators.required),
+    //provinces: new FormControl('', Validators.required),
+    price: new FormControl(1, Validators.required)
   })
 
+  get shippingScope() { return this.shippingFeeForm.get('scope') }
+  get shippingPrice() { return this.shippingFeeForm.get('price') }
+
   
-}
+  ngAfterViewInit(): void {
+    let tooltipInit = new bootstrap.Tooltip(this.tooltip.nativeElement);
+    tooltipInit.enable();
+
+    let prov = this.province.getProvinces()
+    prov.subscribe((response: Province[]) => {
+      this.provinces = response.sort((a, b) => a.name.localeCompare(b.name));
+    })
+  }
+
+  changeProvSelect(province: Province): void {
+    this.provinceSelect = province;
+  }
+
+  addProvince(): void {
+    console.log(this.provinceSelect.name);
+    if(this.provinceSelect) {
+      this.selectedProvinces.push(this.provinceSelect);
+    }
+  }
+
+  removeProvince(province: Province): void {
+    let index = this.selectedProvinces.findIndex(prov => prov == province);
+    this.selectedProvinces.splice(index, 1);
+  }
+
+  postShippingFee(): void {
+    let formdata = new FormData();
+    formdata.append('scope', String(this.shippingScope?.value));
+    formdata.append('price', String(this.shippingPrice?.value));
+
+    if(this.shippingScope?.value == 'specific'){
+      this.selectedProvinces.forEach(province => {
+        formdata.append('provinces[]', province.name)
+      })
+    }
+
+    console.log(formdata);
+  }
+} 
