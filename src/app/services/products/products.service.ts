35f8d187b2014991526849e95e481f76c3bb4089
Mark Angelo Dayrit <markangelodayrit00@gmail.com>
2023-12-16 02:46:02 +0800

    Added Best Seller to Homepage

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product, ProductList, Variant } from 'src/assets/models/products';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { DELETEProductsAdmin, GETMyStylesProducts, GETProductDetails, GETProducts, GETProductsAll, GETProductsByCategory, GETProductsSuggestions, GETProductsTrending, PATCHProductsAdmin, POSTMyStylesRecord, POSTProductsAdmin, GETMyStylesSuggest } from '../endpoints';
+import { DELETEProductsAdmin, GETMyStylesProducts, GETProductDetails, GETProducts, GETProductsAll, GETProductsBestSeller, GETProductsByCategory, GETProductsSuggestions, GETProductsTrending, PATCHProductsAdmin, POSTMyStylesRecord, POSTProductsAdmin, GETMyStylesSuggest } from '../endpoints';
 import { BehaviorSubject, Observable, map, of, shareReplay } from 'rxjs';
 import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
@@ -68,6 +68,12 @@ export class ProductsService {
   getProductsTrending(): Observable<any>{
     // console.log(this.http.get<ProductList>(GETProducts))
     return this.http.get<ProductList>(GETProductsTrending);
+    //return this.http.get<ProductList>('../../assets/sampleData/products.json');
+  }
+
+  getProductsBestSeller(): Observable<any>{
+    // console.log(this.http.get<ProductList>(GETProducts))
+    return this.http.get<ProductList>(GETProductsBestSeller);
     //return this.http.get<ProductList>('../../assets/sampleData/products.json');
   }
     
